52c2d9dbd2094b9c86732dc42790c07139b97eb0
davpoolechem2 <davpoolechem2@gmail.com>
2023-03-16 11:41:21 -0400

    Slight refinement of cpp code

--- nan.cpp
+++ nan.cpp
@@ -16,8 +16,7 @@ void nan_demo_a() {
   std::cout << "sqrt(-1.0) => " << sqrt_neg_one_nan << std::endl;
 
   std::complex<double> sqrt_neg_one_complex = sqrt(std::complex<double>(-1.0));
-  std::cout << "sqrt(complex(-1.0)) => " << sqrt_neg_one_complex << std::endl;
-  std::cout << std::endl;
+  std::cout << "sqrt(complex(-1.0)) => " << sqrt_neg_one_complex << std::endl << std::endl;
 }
 
 void nan_demo_b() { 
@@ -28,8 +27,7 @@ void nan_demo_b() {
   double sqrt_neg_one_nan = sqrt(-1.0);
 
   std::cout << "sqrt(-1.0) == sqrt(-1.0)? " << (sqrt_neg_one_nan == sqrt_neg_one_nan ? "True" : "False") << std::endl;
-  std::cout << "isnan(sqrt(-1.0))? " << (isnan(sqrt_neg_one_nan) ? "True" : "False") << std::endl;
-  std::cout << std::endl;
+  std::cout << "isnan(sqrt(-1.0))? " << (isnan(sqrt_neg_one_nan) ? "True" : "False") << std::endl << std::endl;
 }
   
 void nan_demo_c() { 
@@ -42,12 +40,15 @@ void nan_demo_c() {
 
   std::cout << "sqrt(-1.0) + 1.0 => " << (sqrt_neg_one_nan + 1.0) << std::endl;
   std::cout << "sqrt(-1.0) * 3.0 => " << (sqrt_neg_one_nan*3.0) << std::endl;
-  std::cout << "sqrt(-1.0)^2.0 => " << (std::pow(sqrt_neg_one_nan, 2.0)) << std::endl;
+  std::cout << "sqrt(-1.0)^2.0 => " << (std::pow(sqrt_neg_one_nan, 2.0)) << std::endl << std::endl;
+
+  std::cout << "x = sqrt(-1.0) + 1.0 => " << (sqrt_neg_one_nan + 1.0) << std::endl;
+  std::cout << "y = x * 3.0 => " << ((sqrt_neg_one_nan + 1.0)*3.0) << std::endl;
+  std::cout << "z = y^2.0 => " << (std::pow((sqrt_neg_one_nan + 1.0)*3.0, 2.0)) << std::endl << std::endl;
 
   //std::cout << "sqrt(complex(-1.0)) + 1.0 => " << (sqrt_neg_one_complex + 1.0) << std::endl;
   //std::cout << "sqrt(complex(-1.0)) * 3.0 => " << (sqrt_neg_one_complex*3.0) << std::endl;
   //std::cout << "sqrt(complex(-1.0))^2.0 => " << (std::pow(sqrt_neg_one_complex, 2.0)) << std::endl;
-  std::cout << std::endl;
 }
 
 int main(int argc, char *argv[]) {
