94580b03ab1aaef5acda879a9e1ac5827211b1cf
davpoolechem2 <davpoolechem2@gmail.com>
2023-03-21 14:04:28 -0400

    Add comments to mysqrt

--- mysqrt.cpp
+++ mysqrt.cpp
@@ -2,8 +2,9 @@
 #include <math.h>
 #include <regex>
 
-template <typename T>
-double mysqrt(T input) {
+// homebrewed std::sqrt wrapper inputting double
+// throws an domain_error exception if input is negative
+double mysqrt(double input) {
   if (input >= 0.0) {
     return std::sqrt(input);
   } else {
@@ -11,7 +12,9 @@ double mysqrt(T input) {
   }
 }
 
-template <>
+// homebrewed std::sqrt wrapper inputting a char array
+// uses above wrapper
+// throws an invalid_number exception if input is not a char array representing a number
 double mysqrt(char* input) {
   std::regex is_number {"[-]?[0-9]*[\.[0-9]*]?"};
   if (std::regex_match(input, is_number)) {
@@ -21,13 +24,22 @@ double mysqrt(char* input) {
   }
 }
 
+// test our sqrt wrapper by taking the command line argument as an input
+// exceptions can come from the command line argument being either not a valid number
+// or from the command line argument being a negative number
+// exceptions allow for separate treatment of both cases
+// additionally, exceptions are propogated through the unwinding call stack
+// and are all ultimately handled here
 int main(int argc, char *argv[]) {
+  // lets try taking a square root of our command line argument
   try {
     auto result = mysqrt(argv[1]);
     std::cout << "SUCCESS! Result: " << result << std::endl;
+  // we can get a domain_error from the double-input version of mysqrt...
   } catch (std::domain_error e) {
     std::cout << "Domain error detected! Error: " << std::endl;
     std::cout << e.what() << std::endl;
+  // ..or we can get an invalid_argument exception from the char*-input version...
   } catch (std::invalid_argument e) {
     std::cout << "Invalid argument detected! Error: " << std::endl;
     std::cout << e.what() << std::endl;
